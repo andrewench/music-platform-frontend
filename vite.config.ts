a85759b323a70d83fbcdadaaa119d5a386ed119f
Andrew <an.egens.me@gmail.com>
2023-03-29 07:46:25 +0300

    fix: fix identation in the configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	build: {
-		outDir: 'build',
-	},
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'build',
+  },
 })
