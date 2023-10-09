ba446fadd55943b31d27cc7cbc9ad237dd968040
Andrew <andrewench.react.dev@gmail.com>
2023-10-09 09:04:41 +0300

    add missing async-mutex for custom fetcher

--- app/shared/utils/custom-fetcher.util.ts
+++ app/shared/utils/custom-fetcher.util.ts
@@ -3,6 +3,7 @@ import {
   FetchArgs,
   FetchBaseQueryError,
 } from '@reduxjs/toolkit/dist/query'
+import { Mutex } from 'async-mutex'
 
 import { TokenService } from '@/services'
 
@@ -12,41 +13,57 @@ import { baseQuery } from '@/shared/utils'
 
 import { TAuthSuccessResponse } from '@/shared/types'
 
+const mutex = new Mutex()
+
 export const customFetcher: BaseQueryFn<
   string | FetchArgs,
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
+  await mutex.waitForUnlock()
+
   let response = await baseQuery(args, api, extraOptions)
 
   if (response.error && response.error.status === 401) {
-    const { refreshToken } = TokenService.getTokens()
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
 
-    const refreshResponse = await baseQuery(
-      {
-        url: '/auth/refresh',
-        method: 'POST',
-        body: {
-          refreshToken,
-        },
-      },
-      api,
-      extraOptions
-    )
+      try {
+        const { refreshToken } = TokenService.getTokens()
 
-    if (refreshResponse.data) {
-      const { accessToken, refreshToken } =
-        refreshResponse.data as TAuthSuccessResponse
+        const refreshResponse = await baseQuery(
+          {
+            url: '/auth/refresh',
+            method: 'POST',
+            body: {
+              refreshToken,
+            },
+          },
+          api,
+          extraOptions
+        )
 
-      const { AT_LIFE_TIME, RT_LIFE_TIME } = AppConstant.TOKENS
+        if (refreshResponse.data) {
+          const { accessToken, refreshToken } =
+            refreshResponse.data as TAuthSuccessResponse
 
-      TokenService.setToken('accessToken', accessToken, {
-        expires: new Date(new Date().getTime() + AT_LIFE_TIME),
-      })
+          const { AT_LIFE_TIME, RT_LIFE_TIME } = AppConstant.TOKENS
 
-      TokenService.setToken('refreshToken', refreshToken, {
-        expires: new Date(new Date().getTime() + RT_LIFE_TIME),
-      })
+          TokenService.setToken('accessToken', accessToken, {
+            expires: new Date(new Date().getTime() + AT_LIFE_TIME),
+          })
+
+          TokenService.setToken('refreshToken', refreshToken, {
+            expires: new Date(new Date().getTime() + RT_LIFE_TIME),
+          })
+
+          response = await baseQuery(args, api, extraOptions)
+        }
+      } finally {
+        release()
+      }
+    } else {
+      await mutex.waitForUnlock()
 
       response = await baseQuery(args, api, extraOptions)
     }
