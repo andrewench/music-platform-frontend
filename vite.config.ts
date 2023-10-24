994ec585c1b55fedfab6269cbe5fed8ed8012545
Andrew <andrewench.react.dev@gmail.com>
2023-10-24 21:16:16 +0300

    refactor: update vite.config.ts; use env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [react(), tsPaths()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5183',
-        changeOrigin: true,
-      },
-      '/uploads/images': {
-        target: 'http://localhost:5183',
-        changeOrigin: true,
-      },
+export default defineConfig(() => {
+  const env = loadEnv('all', process.cwd(), '')
+
+  return {
+    plugins: [react(), tsPaths()],
+    build: {
+      outDir: 'build',
     },
-    watch: {
-      usePolling: true,
+    server: {
+      proxy: {
+        '/api': {
+          target: env.SERVER_HOST,
+          changeOrigin: true,
+        },
+        '/uploads/images': {
+          target: env.SERVER_HOST,
+          changeOrigin: true,
+        },
+      },
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: parseInt(env.PORT),
     },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
+  }
 })
